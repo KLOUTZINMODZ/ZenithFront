1187aef08476f6c334f913e6f014275658e9548c
KloutzinModz <wickgames1314@gmail.com>
2025-12-12 17:06:49 -0300

    Klouts testando uns bagui ai

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // Otimização de Performance: Remove console.log em produção
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
